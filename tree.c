cb2c6b1f27b4f3c558e7d47bbd005e646bc09b2e
Benjamin Lannon <lannonbr@clarkson.edu>
2017-03-03 09:11:00 -0500

    Starting work on trees

---
+++ tree.c
@@ -0,0 +1,7 @@
+#include "tree.h"
+
+tree_t * gen_tree() {
+    tree_t * new_tree = malloc(sizeof(tree_t));
+
+    return new_tree;
+}
---
+++ tree.h
@@ -0,0 +1,10 @@
+#ifndef TREE_H
+#define TREE_H
+
+typedef struct tree_s {
+
+} tree_t;
+
+tree_t * gen_tree();
+
+#endif
