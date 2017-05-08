ab90f6c1f52846f5f4a62fd2d6a19ce3b494de27
Benjamin Lannon <lannonbr@clarkson.edu>
2017-05-08 15:23:40 -0400

    Rename pascal programs

--- programs/factorial.s
+++ programs/factorial.s
@@ -9,25 +9,25 @@ main:
 	pushq	%rbp
 	movq	%rsp, %rbp
 	subq	$16, %rsp
-	leaq	-12(%rbp), %rax
+	leaq	-16(%rbp), %rax
 	movq	%rax, %rsi
 	movq	$.LC1, %rdi
 	movq	$0, %rax
 	call	__isoc99_scanf
 	movq	$1, %rax
-	movq	%rax, -8(%rbp)
+	movq	%rax, -12(%rbp)
 	jmp	.L2
 .L3:
-	movq	-8(%rbp), %rax
-	imul	-12(%rbp), %rax
-	movq	%rax, -8(%rbp)
 	movq	-12(%rbp), %rax
-	sub	$1, %rax
+	imul	-16(%rbp), %rax
 	movq	%rax, -12(%rbp)
+	movq	-16(%rbp), %rax
+	sub	$1, %rax
+	movq	%rax, -16(%rbp)
 .L2:
-	cmp	$0, -12(%rbp)
+	cmp	$0, -16(%rbp)
 	jg .L3
-	movq	-8(%rbp), %rax
+	movq	-12(%rbp), %rax
 	movq	%rax, %rsi
 	movq	$.LC0, %rdi
 	movq	$0, %rax
---
+++ programs/proc_with_param.p
@@ -0,0 +1,9 @@
+program main(input, output);
+	procedure foo(a, b: integer);
+	begin
+		write(a);
+		write(b)
+	end;
+begin
+    foo(20, 10)
+end.
